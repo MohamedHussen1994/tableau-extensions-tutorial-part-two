4f99b8f76ef07031776404fa2b7dd9548c8d522b
Mohammed Hussen <mohamedhussen1994@gmail.com>
2020-05-18 18:00:37 +0200

    changing the chart type

--- js/application.js
+++ js/application.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     var ctx = $("#myChart");
     var myChart = new Chart(ctx, {
-       type: 'radar',
+       type: 'polarArea',
        data: {
           labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
           datasets: [{
              label: "Population (millions)",
              backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-             data: [2478,5267,734,784,433]
+             data: [3,7,16,11,14]
           }]
        },
        options: {
